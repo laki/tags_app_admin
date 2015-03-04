0d5193ac626ec196e91e4695b48cd86082bd3d60
Greg Lakomy <greg@bradsdeals.com>
2015-03-04 16:44:23 -0600

    Routes permit only index and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 TagsAppAdmin::Application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :posts
-      resources :tags
+      resources :posts, only: [:index, :create]
+      resources :tags, only: [:index, :create]
     end
   end
 
-  resources :healthcheck, :only => [:index]
+  resources :healthcheck, only: :index
 end
